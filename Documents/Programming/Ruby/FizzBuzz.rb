287a7845c52a6fb872c99c8a002ad3aad3d71247
Andrew Good <drewhgood@gmail.com>
2015-01-22 16:07:23 -0800

    add completed FizzBuzz

--- Documents/Programming/Ruby/FizzBuzz.rb
+++ Documents/Programming/Ruby/FizzBuzz.rb
@@ -1,3 +1,12 @@
-puts "hi"
-
-# this is an update to test git & github
+(1..100).each do |n|
+	if (n % 5 == 0) && (n % 3 == 0) 
+		puts "FizzBuzz"
+	elsif (n % 3 == 0)
+		puts "Fizz"
+	elsif (n % 5 == 0)
+		puts "Buzz"
+	else
+		puts n
+	end
+	
+end
